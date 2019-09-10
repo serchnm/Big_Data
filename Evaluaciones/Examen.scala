774102af0020e137593561b88bb1e23eda5fa7d3
serchnm <sergio.ortiz@tectijuana.edu.mx>
2019-09-09 18:21:56 -0700

    Practicas update

--- Evaluaciones/Examen.scala
+++ Evaluaciones/Examen.scala
@@ -1,15 +1,15 @@
 
 var totalGames = 9
-var games = Array(10,5,20,20,4,5,2,25,1)
-var games2 = Array(3,4,21,36,10,28,35,5,24,42) 
+var game = Array(10,5,20,20,4,5,2,25,1)
+var game2 = Array(3,4,21,36,10,28,35,5,24,42) 
 
-def breakingRecords( games:Array[Int]): List[Int] = {
+def breakingRecords( game:Array[Int]): List[Int] = {
     
-    var max = games(0)
+    var max = game(0)
     var countMax = 0
-    var min = games(0)
+    var min = game(0)
     var countMin = 0
-    for (i <- games){
+    for (i <- game){
 
         if(i > max){
             max = i 
@@ -19,15 +19,12 @@ def breakingRecords( games:Array[Int]): List[Int] = {
             countMin += 1
         }
     }
-
     var myList = List(countMax,countMin)
     return myList
-
-
 }
 
-println(breakingRecords(games))
-println(breakingRecords(games2))
+println(breakingRecords(game))
+println(breakingRecords(game2))
 
 
 
--- Practicas/Practica3.scala
+++ Practicas/Practica3.scala
@@ -1,1 +1,46 @@
+//Versión recursiva descendente
+def fib1(n: Int): Int = {
+  n match {
+    case i if i < 2 => i
+    case i => fib_recur(n-1) + fib_recur(n-2)
+  }
+}
+fib1(6)
 
+//Versión con fórmula explícita
+
+def fibonacciFormula(num: Int): Double = {
+    if(num < 2){
+        return num; 
+    }else{
+        var i = ((1 + math.sqrt(5))/2);
+        var j = ((math.pow(i,num)) - (math.pow((1-i),num)))/ math.sqrt(5);
+        return j;
+    }
+}
+
+//Versión iterativa
+
+def fibonacciIterativa(num: Int): Int = {
+    var a = 0;
+    var b = 1;
+    var c = 0;
+    var k = 1;
+    for(k <- 1 to num){
+        c = b + a;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+//Versión iterativa 2 variable
+def fibonacciIterativa2(num: Int): Int = {
+    var a = 0;
+    var b = 1;
+    for(k <- 1 to num){
+        b = b + a;
+        a = b - a;
+    }
+    return a;
+}

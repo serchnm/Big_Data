8a7ec4a190c93c8a538c757693f51f143be82896
serchnm <sergio.ortiz@tectijuana.edu.mx>
2019-09-09 18:34:06 -0700

    Update branch

--- Evaluaciones/Examen.scala
+++ Evaluaciones/Examen.scala
@@ -1,25 +1,26 @@
 
-var totalGames = 9
+var Input0 = 9
+var Input1 =10
 var game = Array(10,5,20,20,4,5,2,25,1)
 var game2 = Array(3,4,21,36,10,28,35,5,24,42) 
 
 def breakingRecords( game:Array[Int]): List[Int] = {
     
-    var max = game(0)
-    var countMax = 0
-    var min = game(0)
-    var countMin = 0
+    var maxG = game(0)
+    var countMaxG = 0
+    var minG = game(0)
+    var countMinG = 0
     for (i <- game){
 
-        if(i > max){
-            max = i 
-            countMax += 1
-        }else if(i < min){
-            min = i
-            countMin += 1
+        if(i > maxG){
+            maxG = i 
+            countMaxG += 1
+        }else if(i < minG){
+            minG = i
+            countMinG += 1
         }
     }
-    var myList = List(countMax,countMin)
+    var myList = List(countMaxG,countMinG)
     return myList
 }
 

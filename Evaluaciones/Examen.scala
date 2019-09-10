dfa6fcef9e507f96fe62713e756ec66621865fb5
serchnm <sergio.ortiz@tectijuana.edu.mx>
2019-09-09 17:20:09 -0700

    Modificaciones Rama Unidad1

---
+++ Evaluaciones/Examen.scala
@@ -0,0 +1,42 @@
+
+var totalGames = 9
+var games = Array(10,5,20,20,4,5,2,25,1)
+var games2 = Array(3,4,21,36,10,28,35,5,24,42) 
+
+def breakingRecords( games:Array[Int]): List[Int] = {
+    
+    var max = games(0)
+    var countMax = 0
+    var min = games(0)
+    var countMin = 0
+    for (i <- games){
+
+        if(i > max){
+            max = i 
+            countMax += 1
+        }else if(i < min){
+            min = i
+            countMin += 1
+        }
+    }
+
+    var myList = List(countMax,countMin)
+    return myList
+
+
+}
+
+println(breakingRecords(games))
+println(breakingRecords(games2))
+
+
+
+
+
+
+
+
+
+
+
+
--- array.scala
+++
@@ -1,8 +0,0 @@
-val mutmap = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23) ,("Susana", 27))
-
-
-mutmap += ("Miguel" -> 23)
-mutmap
-mutmap.keys
-mutmap.values
-print(mutmap)
